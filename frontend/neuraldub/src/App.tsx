370beed0a9781bf5608c6c3ae94aee5d54541932
Swarup Chanda <swarupchanda1963@gmail.com>
2026-01-24 23:54:22 +0530

    Updates 1

--- frontend/neuraldub/src/App.tsx
+++ frontend/neuraldub/src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
 
 function App() {
-  
 
   return (
     <>
-      <div className='text-bold'>Hii</div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </>
   )
 }
